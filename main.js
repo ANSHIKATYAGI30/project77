6a4e01f53cae34709570ef754239aadcb34f1bc1
ANSHIKATYAGI30 <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2022-07-10 19:52:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var first_name = "Arnav";
+var last_name = "Tyagi";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+var add = a + b;
+console.log(add);
+
+var subtract = a - b;
+console.log(subtract);
+
+var multiply = a * b;
+console.log(multiply);
+
+var divide = a / b;
+console.log(divide);
